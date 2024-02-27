ec6ddfe48e669096f2d9c1c9a2a4b108e83323bb
Sebastian <sebassperez11@gmail.com>
2024-02-27 17:59:40 -0300

    Update Vercel Server

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_react__lBnBkZPi.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/_react__aqDZ7ioZ.mjs').then(n => n._);
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_CxyZ_BWO.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_BOUOMG7I.mjs');
+
+export { page };
