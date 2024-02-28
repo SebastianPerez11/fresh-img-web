c45f467f56b697ae06702bb6403e05e6c4dce9bc
Sebastian <sebassperez11@gmail.com>
2024-02-28 11:21:50 -0300

    Update imgs

--- .netlify/functions-internal/ssr/chunks/_react__DCGV382F.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/_react__BipBr3OJ.mjs').then(n => n._);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/index_CvHGv7w8.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./prerender_DzmMvRu3.mjs');
-
-export { page };
